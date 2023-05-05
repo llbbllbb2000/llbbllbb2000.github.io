586d88a2e407ca67855b8789240dd1d6b7d4b3a9
llbbllbb2000 <zisheng.cai2000@gmail.com>
2023-05-05 00:53:04 -0400

    add color animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,24 @@
 import "./About.scss";
-import React from "react";
+import React, { useState } from "react";
 import Reading from "../../assets/images/reading.jpg";
 import Snowboard from "../../assets/images/snowboard.jpg";
 import Game from "../../assets/images/game.jpg";
 
 const About = () => {
+  const colors = {
+    "item-1" : "#dfc98a",
+    "item-2" : "#7EC8E3",
+    "item-3" : "#acada8"
+  }
+
+  const [colorState, setcolorState] = useState(colors['item-1'])
+
+  const demo = (props) => {
+    setcolorState(colors[props.target.id])
+  }
+
   return (
-    <section id="About">
+    <section id="About" style={{backgroundColor : colorState}}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-12 col-xl-6 about-text">
@@ -24,9 +36,9 @@ const About = () => {
           </div>
           <div className="col-lg-12 col-xl-6 slider-container">
             {/* Using defaultcheck instead of checked in React.js!!! */}
-            <input type="radio" name="slider" id="item-1" defaultChecked/>
-            <input type="radio" name="slider" id="item-2" />
-            <input type="radio" name="slider" id="item-3" />
+            <input type="radio" name="slider" id="item-1" defaultChecked onChange={demo}/>
+            <input type="radio" name="slider" id="item-2" onChange={demo}/>
+            <input type="radio" name="slider" id="item-3" onChange={demo}/>
             <div className="cards">
               <label className="card" for="item-1" id="img-1">
                 <img src={Reading} alt="A photo of book" />
