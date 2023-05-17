8810514931a926cabc507c9a267a0d1bcb75dd1a
llbbllbb2000 <zisheng.cai2000@gmail.com>
2023-05-16 23:08:17 -0400

    add toggler to navbar

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,23 +1,29 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./Title.scss";
 import React from "react";
+import Zimage from "../../assets/images/Z.svg"
 
 const Title = () => {
-  let contents = ["Home", "About", "Education", "Experience", "Future"];
+  let contents = ["About", "Education", "Experience", "Future"];
 
   return (
     <section id="Title">
-      <Navbar collapseOnSelect>
-        <div className="container-fluid">
-          {/* <nav className="navbar navbar-expand-lg navbar-dark">
-          {contents.map(content => (
-            <a className={`nav-item ${content}`} href={`#${content}`}>
-              {content}
-            </a>
-          ))}
-        </nav> */}
-          {/* <Navbar.Toggle aria-controls={""} */}
-        </div>
+      <Navbar expand="md">
+        <Container fluid>
+          <Navbar.Brand href="#" style={{ color: "#E1E1E1" }}>
+            <img className="z-image" src={Zimage} alt="Z" />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="list justify-content-end">
+            <Nav>
+              {contents.map((content) => (
+                <Nav.Link className="item" href={`#${content}`}>
+                  {content}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </section>
   );
